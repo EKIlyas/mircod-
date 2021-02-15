6c943f6d786d97de6802d80bd7b1719752bd131f
Ilias Khabibullin <ilias.khabibullin@tgtdiagnostics.com>
2021-02-15 09:51:42 +0300

    Вторая задачка

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ def left_difference(a_list, b_list):
     return set(a_list) - set(b_list)
 
 
+def remove_zero(a_list):
+    result = [el for el in a_list if el != 0]
+    return result
+
+
 if __name__ == '__main__':
     # Задача 1
     a = [1, 2, 3, 4, 5]
     b = [2, 3, 5, 6, 7]
+    print('Первая задача')
     print(left_difference(a, b))
+
+    # Задача 2
+    c = [0, 1, 0, 3, 0, -1, 10, 0]
+    print('Вторая задача')
+    print(remove_zero(c))
